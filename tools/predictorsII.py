6035ca6da7fb30e6fb7b567615bf95d301b9dd4a
maxmekiska <maxmekiska@gmail.com>
2021-05-25 00:46:49 +0100

    added working neural prophet class wrapper, initial results are rather disappointing compared to the predictors in predictor.py

--- tools/predictorsII.py
+++ tools/predictorsII.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -13,8 +14,39 @@ class UnivariatePredictorII:
 
     def __data_prep(self, data):
         data = data.rename(columns={data.columns[0]: 'y'}, inplace = False)
-        data.index.names = ['df']
+        data = data.reset_index()
+        data = data.rename(columns={'Date': 'ds'}, inplace = False)
         return data
 
+    def fit_model(self, frequency: str):
+        self.model = NeuralProphet()
+        self.details = self.model.fit(self.data, validate_each_epoch=True, freq= frequency)
+
+    def show_performance(self):
+        information = self.details
+
+        plt.subplot(1, 2, 1)
+        plt.plot(information['MAE'])
+        plt.plot(information['MAE_val'])
+        plt.title('Model Mean Average Error')
+        plt.ylabel('MAE')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper right')
+
+
+        plt.subplot(1, 2, 2)
+        plt.plot(information['SmoothL1Loss'])
+        plt.plot(information['SmoothL1Loss_val'])
+        plt.title('Model Loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper right')
+        plt.tight_layout()
+        plt.show()
+
+    def predict(self):
+        prediction = self.model.make_future_dataframe(self.data, periods=self.future)
+        output = self.model.predict(prediction)
+        return output['yhat1']
 
 
