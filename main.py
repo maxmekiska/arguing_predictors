a37ce557a685ec87866d7a2eccada2ea6131f41c
maxmekiska <maxmekiska@gmail.com>
2021-06-02 01:11:17 +0100

    new requierments, minor main changes

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from system.activate import *
 def main(): # experimental main TODO: build loop to verify statistically prediction performance of the system
 
     # data extraction/preparation phase
-    training = DataLoader('aapl', '2009-01-01', '2010-05-01')
-    training = training.get_close()
+    #training = DataLoader('aapl', '2009-01-01', '2010-05-01')
+    #training = training.get_close()
     predict = DataLoader('aapl', '2010-06-01', '2010-09-01')
     predict = predict.get_close()
     predict_req, real = data_prep(predict, 24, 30)
     
     # individual predictors forecasting
-    final_df = individual_predictors1(training, predict_req, 30)
+    final_df = individual_predictors_pretrained1(predict_req, 30)
 
     # system disagreement calculation
     system_disagreement(final_df)
@@ -19,6 +19,14 @@ def main(): # experimental main TODO: build loop to verify statistically predict
     # building of consensus values
     algos = consensus(final_df, real)
     ui = combined_frame(final_df, algos, real)
+    yu = all_stats_frame(ui, final_df)
+
+    correlation(yu, True)
+    mse_score(ui, True)
+    mse_log_score(ui, True)
+    mae_score(ui, True)
+
+
     plot_performance(ui)
 
 if __name__ == "__main__":
