3adce296e5422596c94812816aa76d10e0ac8dce
maxmekiska <maxmekiska@gmail.com>
2021-06-10 20:20:27 +0100

    added new gui to main

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+import PySimpleGUI as sg
+import matplotlib.pyplot as plt
+
 from tools.dataloader import *
 from system.activate import *
 
 def main():
     '''Example main function to execute the system without a jupyter notebook as UI.
     '''
-
     # data extraction/preparation phase
 
     predict = DataLoader('BP', '2018-02-01', '2018-05-01')
     predict = predict.get_adjclose()
+
+    training = DataLoader('BP', '2015-01-01', '2018-01-01')
+    training = training.get_adjclose()
    
     predict_req, real = data_prep(predict, 20, 30) # dividing data into predictor input and real data
 
-    individual_predictors_forecasts = individual_predictors_pretrained_BP_30_5(predict_req, 30)
+    #individual_predictors_forecasts = individual_predictors_pretrained_BP_30_5(predict_req, 30)
 
-    system_disagreement(individual_predictors_forecasts)
+    individual_predictors_forecasts = individual_predictors_template(training, predict_req, 30)
 
     consensus_forecasts = consensus(individual_predictors_forecasts, real)
 
@@ -22,15 +27,36 @@ def main():
 
     prediction_error = absolute_error_analytics(individual_predictors_forecasts, consensus_forecasts, real)
 
-    correlation(prediction_error, True)
+    layout = [[sg.Button('Plot Disagreement'), sg.Button('Plot Correlation'), sg.Cancel()], 
+              [sg.Button('Plot MSE'), sg.Button('Plot MSE Log'), sg.Button('Plot MAE'],
+              [sg.Button('Plot Performance']]
 
-    mse_score(all_forecasts, True)
+    window = sg.Window('System demo', layout)
 
-    mse_log_score(all_forecasts, True)
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Cancel'):
+            break
+        elif event == 'Plot Disagreement':            
+            system_disagreement(individual_predictors_forecasts)
+            plt.show(block=False)
+        elif event == 'Plot Correlation':
+            correlation(prediction_error, True)
+            plt.show(block=False)
+        elif event == 'Plot MSE':
+            mse_score(all_forecasts, True)
+            plt.show(block=False)
+        elif event == 'Plot MSE Log':
+            mse_log_score(all_forecasts, True)
+            plt.show(block=False)
+        elif event == 'MAE':
+            mae_score(all_forecasts, True)
+            plt.show(block=False)
+        elif event == 'Plot Performance':
+            plot_performance(all_forecasts)
+            plt.show(block=False)
 
-    mae = mae_score(all_forecasts, True)
-
-    plot_performance(all_forecasts)
+    window.close()
 
 if __name__ == "__main__":
     main()
