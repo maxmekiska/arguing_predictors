8f6d38a91afb070e5d2b55553c72a2c81f3f8233
maxmekiska <maxmekiska@gmail.com>
2021-06-11 00:59:07 +0100

    minimal gui for plotting results

--- main.py
+++ main.py
@@ -7,10 +7,24 @@ from system.activate import *
 def main():
     '''Example main function to execute the system without a jupyter notebook as UI.
     '''
+
+
+    predict = DataLoader('BP', '2018-02-01', '2018-05-01')
+    predict = predict.get_adjclose()
+
+    training = DataLoader('BP', '2015-01-01', '2018-01-01')
+    training = training.get_adjclose()
+
+    predict_req, real = data_prep(predict, 20, 30) # dividing data into predictor input and real data
+    individual_predictors_forecasts = individual_predictors_template(training, predict_req, 30, 5)
+    consensus_forecasts = consensus(individual_predictors_forecasts, real)
+    all_forecasts = combined_frame(individual_predictors_forecasts, consensus_forecasts, real)
+    prediction_error = absolute_error_analytics(individual_predictors_forecasts, consensus_forecasts, real) 
+
     sg.ChangeLookAndFeel('Dark')      
     sg.SetOptions(element_padding=(1, 1))
 
-    layout = [[sg.Text('Epochs'), sg.InputText()], [sg.Button('Load Data'), sg.Button('Train')], [sg.Button('Plot Disagreement'), sg.Button('Plot Correlation')], 
+    layout = [[sg.Button('Plot Disagreement')], [sg.Button('Plot Correlation')], 
               [sg.Button('Plot MSE'), sg.Button('Plot MSE Log'), sg.Button('Plot MAE')],
               [sg.Button('Plot Performance'), sg.Cancel()]]
 
@@ -20,23 +34,13 @@ def main():
         event, values = window.read()
         if event in (sg.WIN_CLOSED, 'Cancel'):
             break
-        elif event == 'Load Data':            
-            predict = DataLoader('BP', '2018-02-01', '2018-05-01')
-            predict = predict.get_adjclose()
-
-            training = DataLoader('BP', '2015-01-01', '2018-01-01')
-            training = training.get_adjclose()
-        elif event == 'Train':   
-            predict_req, real = data_prep(predict, 20, 30) # dividing data into predictor input and real data
-            individual_predictors_forecasts = individual_predictors_template(training, predict_req, 30, int(values[0]))
-            consensus_forecasts = consensus(individual_predictors_forecasts, real)
-            all_forecasts = combined_frame(individual_predictors_forecasts, consensus_forecasts, real)
-            prediction_error = absolute_error_analytics(individual_predictors_forecasts, consensus_forecasts, real) 
         elif event == 'Plot Disagreement':            
             system_disagreement(individual_predictors_forecasts)
+            plt.tight_layout()
             plt.show(block=False)
         elif event == 'Plot Correlation':
             correlation(prediction_error, True)
+            plt.tight_layout()
             plt.show(block=False)
         elif event == 'Plot MSE':
             mse_score(all_forecasts, True)
