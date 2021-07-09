b7e4e9395b2adbca2eb5697c288786536e956fcd
maxmekiska <maxmekiska@gmail.com>
2021-07-09 15:22:46 +0100

    added future data output into pred input function

--- experimental/predictorsX.py
+++ experimental/predictorsX.py
@@ -293,21 +293,23 @@ class BasicMultivariatePredictor:
         '''
         self.model = keras.models.load_model(location)
 
-def pred_input(stockdatapred: DataFrame, steps_past: int, target: str) -> array:
-    '''Helper function to prepare data input to enable model to make future predictions.
-    
+def pred_input(stockdatapred: DataFrame, steps_past: int, target: str, steps_future: int = 0) -> [(array, DataFrame)]:
+    '''Helper function to prepare data input to enable model to make future predictions. Furthermore, provides real values. 
         Parameters:
             stockdatapred (DataFrame): DataFrame containing all features that shall be considered for predicting the future including the target feature.
             steps_past (int): How much the model needs to look back to make a prediction.
             target (str): Target variable name.
+            steps_future (int): Default to 0 but can be any number of steps into the future.
             
         Returns:
             X (array): Array containing data the model uses to predict into the future.
-    
+            realvalues (DataFrame): DataFrame containing real values. 
     '''
+    realvalues = stockdatapred[target]
     stockdatapred = stockdatapred.drop(target, axis=1)
     stockdatapred = stockdatapred.iloc[0:steps_past]
+    realvalues = realvalues.iloc[steps_past:steps_past+steps_future]
     X = []
     for i in range(len(stockdatapred)):
         X.append(list(stockdatapred.iloc[i]))
-    return array(X)
+    return array(X), pd.DataFrame(realvalues)
